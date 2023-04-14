4617f7500a9648949cb752096fe250e6f8d83045
mo <mo>
2023-04-14 15:07:07 +0800

    目录修改

---
+++ jvm-demo/src/main/java/cn/mo/jvm/demo/FirstTest.java
@@ -0,0 +1,14 @@
+package cn.mo.jvm.demo;
+
+/**
+ * @author mo
+ * @Description jvm测试
+ * @createTime 2023年04月14日 14:50
+ */
+public class FirstTest {
+
+    public static void main(String[] args) {
+        System.out.println("1234454");
+    }
+
+}
